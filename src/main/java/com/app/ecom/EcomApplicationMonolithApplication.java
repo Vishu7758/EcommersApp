00b85ca4be28f64e0f734f975b074e9ecc58630f
Vishal Kharade <vishalkharade@icloud.com>
2025-12-10 00:43:51 +0530

    initial setup

---
+++ src/main/java/com/app/ecom/EcomApplicationMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.app.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomApplicationMonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomApplicationMonolithApplication.class, args);
+    }
+
+}
